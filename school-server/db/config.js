38d4c324aabe7d69c06d8c3ab469fd077bd4fd98
李梓键 <952809310@qq.com>
2020-12-16 11:30:21 +0800

    update school-server/db/config.js.

--- school-server/db/config.js
+++ school-server/db/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // dbs: 'mongodb://139.159.253.110:27017/test1'
-    dbs: 'mongodb+srv://admin:admin@cluster0.axndh.mongodb.net/test0?retryWrites=true&w=majority'
+    dbs: 'mongodb+srv://admin:<passwd>@cluster0.axndh.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
