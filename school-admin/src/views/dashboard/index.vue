6d0d6ca1ed984fd0e3a738486daca055c8e95d31
lzj01231 <git@gitee.com:lzj01231/campus-management-system.git>
2020-12-16 10:56:38 +0800

    123

---
+++ school-admin/src/views/dashboard/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">学校个数：{{schoolCount}}</div>
+    <div class="dashboard-text">学生个数：{{studentCount}}</div>
+    <div class="dashboard-text">班级个数：{{classsCount}}</div>
+    <div class="dashboard-text">老师个数：{{teacherCount}}</div>
+    <div class="dashboard-text">学院个数：{{academyCount}}</div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'Dashboard',
+    computed: {
+      ...mapGetters([
+        'name'
+      ])
+    },
+
+    data() {
+      return {
+        studentCount:'',
+        schoolCount:'',
+        teacherCount:'',
+        academyCount:'',
+        classsCount:''
+      }
+    },
+
+    mounted(){
+      this.$http.post('/api/school/find', this.user).then(res => {
+        console.log('res:', res)
+        console.log('res:'.length)
+        this.schoolCount = res.length
+      })
+      this.$http.post('/api/student/find', this.user).then(res => {
+        console.log('res:', res)
+        console.log('res:'.length)
+        this.studentCount = res.length
+      })
+      this.$http.post('/api/classs/find', this.user).then(res => {
+        console.log('res:', res)
+        console.log('res:'.length)
+        this.classsCount = res.length
+      })
+      this.$http.post('/api/teacher/find', this.user).then(res => {
+        console.log('res:', res)
+        console.log('res:'.length)
+        this.teacherCount = res.length
+      })
+      this.$http.post('/api/academy/find', this.user).then(res => {
+        console.log('res:', res)
+        console.log('res:'.length)
+        this.academyCount = res.length
+      })
+    }
+
+
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
+  }
+</style>
---
+++ school-server/db/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // dbs: 'mongodb://139.159.253.110:27017/test1'
+    dbs: 'mongodb+srv://admin:admin@cluster0.axndh.mongodb.net/test0?retryWrites=true&w=majority'
+}
